48f59b2447593d9807e1e08cff31b7ec87e8493a
Jae-jun Kang <jaykang920@gmail.com>
2013-01-27 23:24:42 +0900

    added xpiler document definitions

--- xpiler/Document.cs
+++ xpiler/Document.cs
@@ -2,8 +2,117 @@
 // See the file COPYING for license details.
 
 using System;
+using System.Collections.Generic;
 
 namespace x2.xpiler {
+  abstract class Def {
+    public const int Namespace = 0;
+    public const int EnumType = 1;
+    public const int CellType = 2;
+    public const int EventType = 3;
+
+    public string name;
+
+    public abstract int Type { get; }
+
+    public Def() {
+      name = null;
+    }
+  }
+
+  class Namespace : Def {
+    public bool opening;
+
+    public override int Type {
+      get { return Namespace; }
+    }
+
+    public Namespace() {
+      opening = false;
+    }
+  }
+
+  class EnumDef : Def {
+    public class Element {
+      public string name;
+      public string value;
+
+      public Element() {
+        name = null;
+        value = null;
+      }
+    }
+
+    public readonly List<Element> elements;
+
+    public override int Type {
+      get { return EnumType; }
+    }
+
+    public EnumDef() {
+      elements = new List<Element>();
+    }
+  }
+
+  class CellDef : Def {
+    public class Property {
+      public string name;
+      public string type;
+      public string defaultValue;
+
+      public string nativeName;
+      public string nativeType;
+
+      public Property() {
+        name = null;
+        type = null;
+        defaultValue = null;
+
+        nativeName = null;
+        nativeType = null;
+      }
+    }
+
+    public string inheritee;
+    public readonly List<Property> properties;
+
+    public override int Type {
+      get { return CellType; }
+    }
+
+    public CellDef() {
+      inheritee = null;
+      properties = new List<Property>();
+    }
+  }
+
+  class EventDef : CellDef {
+    public string id;
+
+    public override int Type {
+      get { return EventType; }
+    }
+
+    public EventDef() {
+      id = null;
+    }
+  }
+
   class Document {
+    public readonly List<Def> defs;
+
+    public string path;
+    public string dirname;
+    public string basename;
+    public string rootNamespace;
+
+    public Document(string path) {
+      defs = new List<Def>();
+
+      this.path = path;
+      dirname = null;
+      basename = null;
+      rootNamespace = null;
+    }
   }
 }
--- xpiler/XmlHandler.cs
+++ xpiler/XmlHandler.cs
@@ -2,10 +2,29 @@
 // See the file COPYING for license details.
 
 using System;
+using System.Xml;
 
 namespace x2.xpiler {
   class XmlHandler : IHandler {
     public Document Handle(string path) {
+      XmlDocument xml = new XmlDocument();
+      try {
+        xml.Load(path);
+      } catch (Exception e) {
+        Console.Error.WriteLine(e.Message);
+        return null;
+      }
+
+      Document doc = new Document(path);
+
+      XmlElement docElem = xml.DocumentElement;
+      Console.WriteLine(docElem.Name);
+      XmlElement rootElem = (XmlElement)docElem.FirstChild;
+      while (rootElem != null && rootElem.IsEmpty == false) {
+        Console.WriteLine(rootElem.Name);
+        rootElem = (XmlElement)rootElem.NextSibling;
+      }
+
       return null;
     }
   }
