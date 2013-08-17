5abf64478a2a2cfa3d68d3fabf601feebb1ced6e
Jae-jun Kang <jaykang920@gmail.com>
2013-08-17 18:38:02 +0900

    added Diagnostics inner class sample

--- x2/Pool.cs
+++ x2/Pool.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace x2
 {
@@ -15,10 +16,18 @@ namespace x2
     public class Pool<T>
         where T : new()
     {
-        private readonly Stack<T> store = new Stack<T>();
+        private readonly Stack<T> store;
+
+        public Pool()
+        {
+            store = new Stack<T>();
+            Diag = new Diagnostics(this);
+        }
 
         public T Acquire()
         {
+            Diag.IncrementAcquireCount();
+
             lock (store)
             {
                 if (store.Count != 0)
@@ -31,10 +40,60 @@ namespace x2
 
         public void Release(T item)
         {
+            Diag.IncrementReleaseCount();
+
             lock (store)
             {
                 store.Push(item);
             }
         }
+
+        #region Diagnostics
+
+        /// <summary>
+        /// Gets the Diagnostics object for this Pool.
+        /// </summary>
+        public Diagnostics Diag { get; private set; }
+
+        /// <summary>
+        /// Internal diagnostics helper class for Pool.
+        /// </summary>
+        public class Diagnostics
+        {
+            private readonly Pool<T> owner;
+
+            private int acquireCount;
+            private int releaseCount;
+
+            public int AcquireCount { get { return acquireCount; } }
+            public int ReleaseCount { get { return releaseCount; } }
+
+            public int StoreSize {
+                get
+                {
+                    lock (owner.store)
+                    {
+                        return owner.store.Count;
+                    }
+                }
+            }
+
+            internal Diagnostics(Pool<T> owner)
+            {
+                this.owner = owner;
+            }
+
+            public void IncrementAcquireCount()
+            {
+                Interlocked.Increment(ref acquireCount);
+            }
+
+            public void IncrementReleaseCount()
+            {
+                Interlocked.Increment(ref releaseCount);
+            }
+        }
+
+        #endregion
     }
 }
