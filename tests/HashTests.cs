2f9ea557aa1cb5603658a54e11e12acb59d7e0f4
Jae-jun Kang <jaykang920@gmail.com>
2013-08-21 20:59:07 +0900

    added NUnit test assembly project

---
+++ tests/HashTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using x2;
+
+namespace x2.Tests
+{
+    [TestFixture]
+    public class HashTests
+    {
+        [Test]
+        public void TestCreation()
+        {
+            Hash hash = new Hash(17);
+            Assert.NotNull(hash);
+        }
+    }
+}
