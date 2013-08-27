cc64e4faadf8428ddca9b2c338a4a721e8e1c9d3
Jae-jun Kang <jaykang920@gmail.com>
2013-08-27 18:18:16 +0900

    added skeletal second sample

---
+++ samples/cap/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace x2.Samples.Capitalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "exit" || line == "quit")
+                {
+                    break;
+                }
+
+                Console.WriteLine(Capitalize(line));
+            }
+        }
+
+        static string Capitalize(string s)
+        {
+            return s.ToUpper();
+        }
+    }
+}
