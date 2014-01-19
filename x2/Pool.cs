254e93c1314a314afd93acfbfadf3b801d6a3bc7
Jae-jun Kang <jaykang920@gmail.com>
2014-01-19 14:44:38 +0900

    commented Pool(T) class

--- x2/Pool.cs
+++ x2/Pool.cs
@@ -18,20 +18,42 @@ namespace x2
         private readonly Stack<T> store;
         private readonly int capacity;
 
+        /// <summary>
+        /// Gets the maximum number of objects that can be contained in the pool.
+        /// </summary>
         public int Capacity { get { return capacity; } }
+
+        /// <summary>
+        /// Gets the number of objects contained in the pool.
+        /// </summary>
         public int Count { get { return store.Count; } }
 
+        /// <summary>
+        /// Initializes a new instance of the Pool(T) class, without a capacity
+        /// limit.
+        /// </summary>
         public Pool()
         {
             store = new Stack<T>();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the Pool(T) class, with the specified
+        /// maximum capacity.
+        /// </summary>
+        /// <param name="capacity"></param>
         public Pool(int capacity)
         {
             store = new Stack<T>(capacity);
             this.capacity = capacity;
         }
 
+        /// <summary>
+        /// Tries to pop an object out of the pool.
+        /// </summary>
+        /// <returns>
+        /// The object removed from the pool, or null if the pool is empty.
+        /// </returns>
         public T Pop()
         {
             lock (store)
@@ -44,6 +66,13 @@ namespace x2
             return null;
         }
 
+        /// <summary>
+        /// Tries to push the specified object into the pool.
+        /// </summary>
+        /// <remarks>
+        /// If the pool has a non-zero capacity limit, the object may be dropped
+        /// when the number of pooled objects reaches the capacity.
+        /// </remarks>
         public void Push(T item)
         {
             if (item == null)
