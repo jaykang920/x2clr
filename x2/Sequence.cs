b2fb0e81bda0864d2a1878d6eb5f56a7626bbdaa
Jae-jun Kang <jaykang920@gmail.com>
2014-03-12 18:09:38 +0900

    let Sequence(T) implement IList(T)

--- x2/Sequence.cs
+++ x2/Sequence.cs
@@ -8,16 +8,21 @@ using System.Text;
 
 namespace x2
 {
-    public class Sequence<T> : Cell, IEnumerable<T> where T : Cell, new()
+    public class Sequence<T> : Cell, IList<T> where T : Cell, new()
     {
         private readonly List<T> store;
 
         public int Count { get { return store.Count; } }
+        public bool IsReadOnly { get { return false; } }
 
         /// <summary>
         /// Gets the value of the item at the specified index.
         /// </summary>
-        public T this[int index] { get { return store[index]; } }
+        public T this[int index]
+        {
+            get { return store[index]; }
+            set { store[index] = value; }
+        }
 
         public Sequence()
             : base(0)
@@ -25,16 +30,61 @@ namespace x2
             store = new List<T>();
         }
 
-        public Sequence<T> Add(T item)
+        public void Add(T item)
+        {
+            store.Add(item);
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            store.AddRange(collection);
+        }
+
+        public Sequence<T> Append(T item)
         {
             store.Add(item);
             return this;
         }
 
-        public Sequence<T> AddRange(IEnumerable<T> collection)
+        public Sequence<T> AppendRange(IEnumerable<T> collection)
         {
             store.AddRange(collection);
             return this;
+        }
+
+        public void Clear()
+        {
+            store.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return store.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            store.CopyTo(array, arrayIndex);
+        }
+
+        public int IndexOf(T item)
+        {
+            return store.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            store.Insert(index, item);
+        }
+
+        public bool Remove(T item)
+        {
+            return store.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            store.RemoveAt(index);
         }
 
         public IEnumerator<T> GetEnumerator()
