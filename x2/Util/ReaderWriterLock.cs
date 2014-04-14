6e0775c807dfc1b456f41173eb75aed82d994fa9
Jae-jun Kang <jaykang920@gmail.com>
2014-04-14 12:33:32 +0900

    added scoped Read/WriteLock utility classes

---
+++ x2/Util/ReaderWriterLock.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2013 Jae-jun Kang
+// See the file COPYING for license details.
+
+using System;
+using System.Threading;
+
+namespace x2
+{
+    public class ReadLockSlim : IDisposable
+    {
+        private readonly ReaderWriterLockSlim rwlock;
+
+        public ReadLockSlim(ReaderWriterLockSlim rwlock)
+        {
+            this.rwlock = rwlock;
+            rwlock.EnterReadLock();
+        }
+
+        public void Dispose()
+        {
+            rwlock.ExitReadLock();
+        }
+    }
+
+    public class WriteLockSlim : IDisposable
+    {
+        private readonly ReaderWriterLockSlim rwlock;
+
+        public WriteLockSlim(ReaderWriterLockSlim rwlock)
+        {
+            this.rwlock = rwlock;
+            rwlock.EnterWriteLock();
+        }
+
+        public void Dispose()
+        {
+            rwlock.ExitWriteLock();
+        }
+    }
+}
