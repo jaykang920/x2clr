d91c24513d2709465c65a3bc35ad6076d7ca707a
Jae-jun Kang <jaykang920@gmail.com>
2015-03-13 19:06:27 +0900

    added StringSerializer

---
+++ x2/Serializers/StringSerializer.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) 2013-2015 Jae-jun Kang
+// See the file COPYING for license details.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace x2
+{
+    /// <summary>
+    /// Text string serializer.
+    /// </summary>
+    public abstract class StringSerializer : VerboseSerializer
+    {
+        protected StringBuilder stringBuilder;
+
+        protected StringSerializer(StringBuilder stringBuilder)
+        {
+            this.stringBuilder = stringBuilder;
+        }
+
+        public override string ToString()
+        {
+            return stringBuilder.ToString();
+        }
+
+        // Name-value pair writers for primitive types
+        public abstract void Write(string name, bool value);
+        public abstract void Write(string name, byte value);
+        public abstract void Write(string name, sbyte value);
+        public abstract void Write(string name, short value);
+        public abstract void Write(string name, int value);
+        public abstract void Write(string name, long value);
+        public abstract void Write(string name, float value);
+        public abstract void Write(string name, double value);
+        public abstract void Write(string name, string value);
+        public abstract void Write(string name, DateTime value);
+
+        // Name-value pair writers for composite types
+        public abstract void Write(string name, byte[] value);
+        public abstract void Write(string name, List<int> value);
+        public abstract void Write<T>(string name, List<T> value) where T : Cell;
+        public abstract void Write<T>(string name, T value) where T : Cell;
+
+        // Value writers for primitive types
+        public void Write(bool value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(byte value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(sbyte value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(short value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(int value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(long value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(float value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(double value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(string value)
+        {
+            stringBuilder.Append(value);
+        }
+        public void Write(DateTime value)
+        {
+            stringBuilder.Append(value.ToString());
+        }
+
+        // Value writers for composite types
+        public abstract void Write(byte[] value);
+        public abstract void Write(List<int> value);
+        public abstract void Write<T>(List<T> value) where T : Cell;
+        public abstract void Write<T>(T value) where T : Cell;
+    }
+}
