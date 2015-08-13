88662b108081bab2d666f2c27365ef796736a2b1
Jae-jun Kang <jaykang920@gmail.com>
2015-08-13 15:37:48 +0900

    merged pull request

--- x2/VerboseSerializer.cs
+++ x2/VerboseSerializer.cs
@@ -9,44 +9,42 @@ namespace x2
     /// <summary>
     /// Defines methods to write name-value pairs into the backing object.
     /// </summary>
-    public abstract class VerboseSerializer
+    public interface VerboseSerializer
     {
         // Name-value pair writers for primitive types
-        public abstract void Write(string name, bool value);
-        public abstract void Write(string name, byte value);
-        public abstract void Write(string name, sbyte value);
-        public abstract void Write(string name, short value);
-        public abstract void Write(string name, int value);
-        public abstract void Write(string name, long value);
-        public abstract void Write(string name, float value);
-        public abstract void Write(string name, double value);
-        public abstract void Write(string name, string value);
-        public abstract void Write(string name, DateTime value);
+        void Write(string name, bool value);
+        void Write(string name, byte value);
+        void Write(string name, sbyte value);
+        void Write(string name, short value);
+        void Write(string name, int value);
+        void Write(string name, long value);
+        void Write(string name, float value);
+        void Write(string name, double value);
+        void Write(string name, string value);
+        void Write(string name, DateTime value);
 
         // Name-value pair writers for composite types
-        public abstract void Write(string name, byte[] value);
-        public abstract void Write(string name, List<int> value);
-        public abstract void Write(string name, List<List<int>> value);
-        public abstract void Write<T>(string name, List<T> value) where T : Cell;
-        public abstract void Write<T>(string name, T value) where T : Cell;
+        void Write(string name, byte[] value);
+        void Write(string name, List<int> value);
+        void Write<T>(string name, List<T> value) where T : Cell;
+        void Write<T>(string name, T value) where T : Cell;
 
         // Value writers for primitive types
-        public abstract void Write(bool value);
-        public abstract void Write(byte value);
-        public abstract void Write(sbyte value);
-        public abstract void Write(short value);
-        public abstract void Write(int value);
-        public abstract void Write(long value);
-        public abstract void Write(float value);
-        public abstract void Write(double value);
-        public abstract void Write(string value);
-        public abstract void Write(DateTime value);
+        void Write(bool value);
+        void Write(byte value);
+        void Write(sbyte value);
+        void Write(short value);
+        void Write(int value);
+        void Write(long value);
+        void Write(float value);
+        void Write(double value);
+        void Write(string value);
+        void Write(DateTime value);
 
         // Value writers for composite types
-        public abstract void Write(byte[] value);
-        public abstract void Write(List<int> value);
-        public abstract void Write(List<List<int>> value);
-        public abstract void Write<T>(List<T> value) where T : Cell;
-        public abstract void Write<T>(T value) where T : Cell;
+        void Write(byte[] value);
+        void Write(List<int> value);
+        void Write<T>(List<T> value) where T : Cell;
+        void Write<T>(T value) where T : Cell;
     }
 }
