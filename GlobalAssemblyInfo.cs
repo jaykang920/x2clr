aa58a4e696ff4156afff940bbe7e0f8aeeb35f4a
Jae-jun Kang <jaykang920@gmail.com>
2015-09-11 01:48:31 +0900

    version 0.5.6

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("0.5.4.0")]
-[assembly: AssemblyFileVersion("0.5.4.0")]
+[assembly: AssemblyVersion("0.5.6.0")]
+[assembly: AssemblyFileVersion("0.5.6.0")]
