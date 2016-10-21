42457f77cdca03147f0aeada242441c91e11aa63
Jae-jun Kang <jaykang920@gmail.com>
2016-10-21 15:04:53 +0900

    added ConcurrentQueue flows by @keedongpark

---
+++ x2/Flows/ConcurrentQueueFlow.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2013-2016 Jae-jun Kang
+// See the file LICENSE for details.
+
+// Contributed by keedongpark
+
+#if NET40
+using System;
+using System.Collections.Concurrent;
+
+namespace x2
+{
+    /// <summary>
+    /// Abstract base class for event-based concurrent flow
+    /// </summary>
+    public abstract class ConcurrentQueueFlow : Flow
+    {
+        protected ConcurrentQueue<Event> queue;
+        protected readonly object syncRoot;
+
+        protected ConcurrentQueueFlow()
+        {
+            queue = new ConcurrentQueue<Event>();
+            syncRoot = new Object();
+        }
+
+        public override void Feed(Event e)
+        {
+            queue.Enqueue(e);
+        }
+
+        protected override void OnHeartbeat()
+        {
+            int length = queue.Count;
+            if (length >= LongQueueLogThreshold)
+            {
+                Log.Emit(LongQueueLogLevel, "{0} queue length {1}", name, length);
+            }
+        }
+    }
+}
+#endif
